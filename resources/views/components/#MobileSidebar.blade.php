485b33e685a57e4b436c5ae5bc3ea8c47006861c
HoshmandG90 <hoshmandG900@gmail.com>
2021-10-15 09:23:00 +0300

    added Components File

---
+++ resources/views/components/#MobileSidebar.blade.php
@@ -0,0 +1,93 @@
+<div class="py-4 text-gray-500 dark:text-gray-400">
+    <a class="ml-6 flex  font-bold text-gray-800 dark:text-gray-200" href="#">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clip-rule="evenodd" />
+          </svg> <span class="ml-1"> Messenger</span>
+    </a>
+    <ul class="mt-6">
+        <li class="relative px-6 py-3">
+            <span class="absolute inset-y-2 left-0 w-1 {{request()->is('home') ? 'bg-purple-600 rounded-tr-lg rounded-br-lg' : ''}} "
+                aria-hidden="true"></span>
+            <a  href="{{route('home')}}"class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100">
+                <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
+                    stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                    <path
+                        d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6">
+                    </path>
+                </svg>
+                <span class="ml-4">Home</span>
+            </a>
+        </li>
+        <li class="relative px-6 py-3">
+            <span class="absolute inset-y-0 left-0 w-1 {{request()->is('Explore') ? 'bg-purple-600 rounded-tr-lg rounded-br-lg' : ''}} "
+                aria-hidden="true"></span>
+            <a class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
+                href="{{route('Explore')}}">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z" />
+                  </svg>
+                <span class="ml-4">Explore</span>
+            </a>
+        </li>
+        <li class="relative px-6 py-3">
+            <span class="absolute inset-y-0 left-0 w-1 "
+                aria-hidden="true"></span>
+            <a class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
+                href="index.html">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                  </svg>
+                <span class="ml-4">Notifications</span>
+            </a>
+        </li>
+        <li class="relative px-6 py-3">
+            <span class="absolute inset-y-0 left-0 w-1  "
+                aria-hidden="true"></span>
+            <a class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
+                href="">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clip-rule="evenodd" />
+                  </svg>
+                <span class="ml-4">Messages</span>
+            </a>
+        </li>
+
+        <li class="relative px-6 py-3">
+            <span class="absolute inset-y-0 left-0 w-1 "
+                aria-hidden="true"></span>
+            <a class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
+                href="index.html">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path d="M5 4a2 2 0 012-2h6a2 2 0 012 2v14l-5-2.5L5 18V4z" />
+                  </svg>
+                <span class="ml-4">Bookmarks</span>
+            </a>
+        </li>
+
+        <li class="relative px-6 py-3">
+            <span class="absolute inset-y-2 left-0 w-1 {{request()->is('friend') ? 'bg-purple-600 rounded-tr-lg rounded-br-lg' : ''}}"
+                aria-hidden="true"></span>
+            <a href="{{route('friend')}}" class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M3 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
+                  </svg>
+                <span class="ml-4">Lists</span>
+            </a>
+        </li>
+        <li class="relative px-6 py-3">
+            <span class="absolute inset-y-0 left-0 w-1 {{request()->is('profile') ? 'bg-purple-600 rounded-tr-lg rounded-br-lg' : ''}}  "
+                aria-hidden="true"></span>
+            <a href="{{route('profile',auth()->user()->username)}}" class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
+                >
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M18 3a1 1 0 00-1.447-.894L8.763 6H5a3 3 0 000 6h.28l1.771 5.316A1 1 0 008 18h1a1 1 0 001-1v-4.382l6.553 3.276A1 1 0 0018 15V3z" clip-rule="evenodd" />
+                  </svg>
+                <span class="ml-4">Profile</span>
+            </a>
+        </li>
+
+        <livewire:admin.logout/>
+    </ul>
+ 
+   
+</div>
